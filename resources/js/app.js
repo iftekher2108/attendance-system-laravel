2ad8dcff351546fce214898c50b94cc93edc52c8
iftekher2108 <parvezkhan21082002@gmail.com>
2024-07-04 17:48:31 +0600

    attendance system

---
+++ resources/js/app.js
@@ -0,0 +1,35 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
+import Select from 'primevue/select';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .use(PrimeVue)
+            .use(ToastService)
+            .component('InputText', InputText)
+            .component('Textarea', Textarea)
+            .component('Select', Select)
+            .component('Toast', Toast)
+            .mount(el);
+    },
+    progress: {
+        color: '#06D001',
+    },
+});
